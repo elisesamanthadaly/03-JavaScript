3f186f5d1e470f3cbcb8718c642f169d508776cb
Elise Daly <elise.samantha.daly@gmail.com>
2021-02-21 14:17:00 -0500

    Adding blank submission password length scenario

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function writePassword() {
     //Initial prompt for length choice
     var lengthChoice = prompt("How many characters would you like your password to contain? Please input a value between 8 and 128, using numeric characters.");
 
-    //Repeats length prompt if user selects an invalid option (cancel, non-numeric input, out-of-range input)
+    //Repeats length prompt if user selects an invalid option (cancel/blank submission, non-numeric input, out-of-range input)
     while (lengthChoice === null || isNaN(lengthChoice) || lengthChoice < 8 || lengthChoice > 128) {
-      if (lengthChoice === null) {
+      if (lengthChoice === null || lengthChoice === "") {
         alert("Please input a password length.");
       }
       else if (isNaN(lengthChoice)) {
